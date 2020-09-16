4bfab040ad8e79784b9d5c03de32e35f758b2e4d
sheiun <me.sheiun@gmail.com>
2020-09-16 14:52:53 +0800

    Fix icons for andriod

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -124,26 +124,11 @@ cfg.module.rules.push({
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#323333',
         icons: [
           {
             src: 'icons/icon-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-256x256.png',
-            sizes: '256x256',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-384x384.png',
-            sizes: '384x384',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-512x512.png',
-            sizes: '512x512',
             type: 'image/png'
           }
         ]

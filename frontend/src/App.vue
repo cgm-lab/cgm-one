743a58b0e08f5f834a6a4fad9c9efadd7fa1b2a0
sheiun <me.sheiun@gmail.com>
2020-09-16 17:55:58 +0800

    Add preload page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,25 @@
 <template>
-  <div id="q-app">
+  <div id="q-app" :class="loadClass">
+    <Loading />
     <router-view />
   </div>
 </template>
 <script>
+import Loading from './components/Loading';
+
 export default {
   name: 'App',
+  components: { Loading },
+  data: () => ({ loadClass: '' }),
   created() {
-    this.$q.loading.show();
+    this.load();
     try {
       this.$q.dark.set(this.$q.localStorage.getItem('dark'));
     } catch (e) {
       // data wasn't successfully saved due to
       // a Web Storage API error
     }
-    this.fetchMetrics().then(() => this.$q.loading.hide());
+    this.fetchMetrics().then(() => setTimeout(this.loaded, 1000));
 
     setInterval(() => {
       this.$q.loading.show();
@@ -23,6 +28,12 @@ export default {
     }, 60 * 5 * 1000);
   },
   methods: {
+    load() {
+      this.loadClass = '';
+    },
+    loaded() {
+      this.loadClass = 'loaded';
+    },
     async fetchMetrics() {
       let res = await fetch('https://one.cgm.im/api/metrics');
       let data = await res.json();
